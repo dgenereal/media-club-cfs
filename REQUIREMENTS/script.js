24046deac798a71f940becbb595abd128352a394
ardabozdere <158765051+ardabozdere@users.noreply.github.com>
2024-02-03 17:22:28 +0000

    Add files via upload

---
+++ REQUIREMENTS/script.js
@@ -0,0 +1,20 @@
+function openMiniMoviesPage() {
+  window.location.href = 'subpages/minimovies/minimovies.html';
+}
+  //Derrick this is an OnClick Function u prolly know but like better to write notes then not to
+
+function OpenAdvertisementsPage() {
+  window.location.href = 'subpages/ads/advertisements.html';
+}
+
+function backMainMenu() {
+  window.location.href = '../../index.html'; // .. means go back a directory so i have ../../ so it sgoes back to subpages then to the main pages but yh
+}
+
+function openCFS() {
+  window.location.href = 'https://www.chingfordfoundation.org';
+}
+
+function openCastList() {
+  window.location.href = '../subpages/minimovies/subpages/cast.html';
+}
---
+++ subpages/minimovies/script.js
@@ -0,0 +1,11 @@
+function backMainMenu() {
+  window.location.href = '../../index.html'; // .. means go back a directory so i have ../../ so it sgoes back to subpages then to the main pages but yh
+}
+
+function openCastList() {
+  window.location.href = 'subpages/cast.html';
+}
+
+function back() {
+  window.location.href = '../minimovies.html';
+}
